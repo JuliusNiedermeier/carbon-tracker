1541d3c04eeebd0870e1c74eb55222bf68732bf3
JuiusNiedermeier <juius.niedermeier@outlook.de>
2023-09-20 12:28:37 +0200

    Removed unused Neon env variable

--- src/common/env.ts
+++ src/common/env.ts
@@ -6,7 +6,6 @@ import "dotenv/config";
 const EnvSchema = object({
   NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string(),
   CLERK_SECRET_KEY: string(),
-  NEON_CONNECTION: string(),
   SUPABASE_POOLED_DB_CONNECTION_STRING: string(),
   OPENAI_API_KEY: string(),
 });
