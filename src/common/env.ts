017f2f18107e8a7b9ecc1a35301991cbbacf1b44
JuiusNiedermeier <juius.niedermeier@outlook.de>
2023-09-20 17:47:14 +0200

    Instaled an configured supabase client and helpers

--- src/common/env.ts
+++ src/common/env.ts
@@ -6,6 +6,8 @@ import "dotenv/config";
 const EnvSchema = object({
   NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string(),
   CLERK_SECRET_KEY: string(),
+  NEXT_PUBLIC_SUPABASE_URL: string(),
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: string(),
   SUPABASE_POOLED_DB_CONNECTION_STRING: string(),
   OPENAI_API_KEY: string(),
 });
