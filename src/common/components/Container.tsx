99c2b76576f13319e17f500dbf87638d084f4805
clem <mail@fait.de>
2023-10-30 18:59:11 +0100

    Remove Max Width of Table for max. Width

--- src/common/components/Container.tsx
+++ src/common/components/Container.tsx
@@ -5,7 +5,7 @@ import { type HTMLAttributes } from "react";
 export interface ContainerProps extends HTMLAttributes<HTMLDivElement> {}
 
 export const Container = forwardRef<HTMLDivElement, PropsWithChildren<ContainerProps>>(({ className, ...props }, ref) => {
-  return <div className={cn("max-w-[90rem] mx-auto px-4", className)} {...props} ref={ref} />;
+  return <div className={cn("mx-auto px-4", className)} {...props} ref={ref} />;
 });
 
 Container.displayName = "Container";
