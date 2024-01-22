119a137f71d0ded7b6aadc32b2834527f121b912
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-01-23 00:46:20 +0100

    Exposed native input props for input-cell

--- optimistic/app/(routes)/[company]/_components/table-utils/cells/input-cell.tsx
+++ optimistic/app/(routes)/[company]/_components/table-utils/cells/input-cell.tsx
@@ -1,22 +1,23 @@
+import { cn } from "@/app/_utils/cn";
 import { ComponentProps, FC, startTransition, useCallback, useEffect, useState } from "react";
 
-type Props = {
+type Props = ComponentProps<"input"> & {
   value: string;
   onUpdate: (value: string) => void;
 };
 
-export const InputCell: FC<Props> = (props) => {
-  const [internalValue, setInternalValue] = useState(props.value);
+export const InputCell: FC<Props> = ({ value, onUpdate, className, ...props }) => {
+  const [internalValue, setInternalValue] = useState(value);
 
-  useEffect(() => setInternalValue(props.value), [props.value]);
+  useEffect(() => setInternalValue(value), [value]);
 
   const handleInput = useCallback<Required<ComponentProps<"input">>["onInput"]>(
     (e) => {
       setInternalValue(e.currentTarget.value);
-      startTransition(() => props.onUpdate(e.currentTarget.value));
+      startTransition(() => onUpdate(e.currentTarget.value));
     },
-    [props.onUpdate]
+    [onUpdate]
   );
 
-  return <input className="cell" value={internalValue} onInput={handleInput} spellCheck={false} />;
+  return <input className={cn("cell", className)} value={internalValue} onInput={handleInput} spellCheck={false} {...props} />;
 };
