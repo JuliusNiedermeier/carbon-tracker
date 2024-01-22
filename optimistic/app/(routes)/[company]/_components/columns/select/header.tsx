8221cec5fbb79a4857fecb24f7e2564dec1f4f75
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-01-22 22:57:04 +0100

    Connected checkboxes to table state

--- optimistic/app/(routes)/[company]/_components/columns/select/header.tsx
+++ optimistic/app/(routes)/[company]/_components/columns/select/header.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import { ActivityDisplayHeaderContext, ActivityHeaderContext } from "../../../_utils/cell-types";
-import { Header } from "../../table-utils/headers/header";
 import { Checkbox } from "@/app/_components/ui/checkbox";
 
 export const SelectHeader: FC<ActivityDisplayHeaderContext> = (props) => {
   return (
     <div className="header">
-      <Checkbox />
+      <Checkbox
+        checked={props.table.getIsSomeRowsSelected() ? "indeterminate" : props.table.getIsAllRowsSelected()}
+        onCheckedChange={(checked) => props.table.toggleAllRowsSelected(!!checked)}
+      />
     </div>
   );
 };
