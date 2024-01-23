ab2bf3e7d31bdb8f72cfca0f2abcaf56b3bdf3d7
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-01-23 15:24:50 +0100

    Enhance BaseCell with styling props
    Updated the BaseCell component to support additional styling options,
    including dynamic padding and customizable classes. This change
    increases the flexibility and reusability of the BaseCell across the
    application by allowing external styling to be easily integrated.

--- optimistic/app/(routes)/[company]/_components/table-utils/cells/base-cell.tsx
+++ optimistic/app/(routes)/[company]/_components/table-utils/cells/base-cell.tsx
@@ -1,5 +1,22 @@
-import { FC, PropsWithChildren } from "react";
+import { cn } from "@/app/_utils/cn";
+import { ComponentProps, FC, PropsWithChildren } from "react";
 
-export const BaseCell: FC<PropsWithChildren> = ({ children }) => {
-  return <div className="cell">{children}</div>;
+type Props = ComponentProps<"div"> & {
+  padding?: boolean;
+};
+
+export const BaseCell: FC<PropsWithChildren<Props>> = ({ children, padding, className, ...props }) => {
+  return (
+    <div
+      className={cn(
+        "flex-1 w-64",
+        "relative flex h-full items-center bg-transparent whitespace-nowrap overflow-hidden text-ellipsis cursor-default text-sm text-gray-600 border-r border-gray-300 last:border-r-0 focus-within:ring-2 focus-within:ring-gray-600 focus-within:!border-transparent hover:bg-gray-200 focus-within:bg-gray-200 focus-within:z-10 focus-within:rounded-[1px] focus-within:text-gray-800 focus-within:font-medium focus-within:cursor-auto outline-none",
+        { "px-3": padding ?? true },
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
 };
