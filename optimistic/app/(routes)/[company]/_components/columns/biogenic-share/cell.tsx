24955559c1bc68bf45f92385a45cb752973ff52e
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-01-31 17:08:06 +0100

    Added update functionality to double counting and biogenic share columns

--- optimistic/app/(routes)/[company]/_components/columns/biogenic-share/cell.tsx
+++ optimistic/app/(routes)/[company]/_components/columns/biogenic-share/cell.tsx
@@ -1,12 +1,16 @@
-import { FC } from "react";
+import { ComponentProps, FC } from "react";
 import { Cell } from "@/app/(routes)/[company]/_components/cell";
 import { ActivityCellContext } from "@/app/(routes)/[company]/_utils/cell-types";
 import { Checkbox } from "@/app/_components/ui/checkbox";
 
 export const BiogenicShareCell: FC<ActivityCellContext<"biogenicShare">> = (props) => {
+  const handleCheckedChange: ComponentProps<typeof Checkbox>["onCheckedChange"] = (checked) => {
+    props.table.options.meta?.updateCell(props.row.original.id, "biogenicShare", !!checked);
+  };
+
   return (
     <Cell width={props.column.getSize()}>
-      <Checkbox></Checkbox>
+      <Checkbox checked={!!props.getValue()} onCheckedChange={handleCheckedChange} />
     </Cell>
   );
 };
--- optimistic/app/(routes)/[company]/_components/columns/double-counting/cell.tsx
+++ optimistic/app/(routes)/[company]/_components/columns/double-counting/cell.tsx
@@ -1,12 +1,16 @@
-import { FC } from "react";
+import { ComponentProps, FC } from "react";
 import { Cell } from "@/app/(routes)/[company]/_components/cell";
 import { ActivityCellContext } from "@/app/(routes)/[company]/_utils/cell-types";
 import { Checkbox } from "@/app/_components/ui/checkbox";
 
 export const DoubleCountingCell: FC<ActivityCellContext<"doubleCounting">> = (props) => {
+  const handleCheckedChange: ComponentProps<typeof Checkbox>["onCheckedChange"] = (checked) => {
+    props.table.options.meta?.updateCell(props.row.original.id, "doubleCounting", !!checked);
+  };
+
   return (
     <Cell width={props.column.getSize()}>
-      <Checkbox></Checkbox>
+      <Checkbox checked={!!props.getValue()} onCheckedChange={handleCheckedChange} />
     </Cell>
   );
 };
