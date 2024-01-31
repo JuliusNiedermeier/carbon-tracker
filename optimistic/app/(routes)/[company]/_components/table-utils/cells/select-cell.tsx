0cde3e788e16fc4be253ee947945e6e3a2e06a51
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-01-31 15:21:36 +0100

    Implemented the new onClear prop in the table utility select-cell

--- optimistic/app/(routes)/[company]/_components/table-utils/cells/select-cell.tsx
+++ optimistic/app/(routes)/[company]/_components/table-utils/cells/select-cell.tsx
@@ -4,18 +4,23 @@ import { ComponentProps, FC, ReactNode, useState } from "react";
 
 type Props = {
   value: string;
-  onValueChange: ComponentProps<typeof Select>["onValueChange"];
+  onValueChange?: (value: string | null) => any;
   options: { value: string; component: ReactNode }[];
 };
 
 export const SelectCell: FC<Props> = (props) => {
   const [open, setOpen] = useState(false);
 
+  const handleClear: ComponentProps<typeof SelectContent>["onClear"] = () => {
+    setOpen(false);
+    props.onValueChange?.(null);
+  };
+
   return (
     <Select open={open} onOpenChange={setOpen} value={props.value} onValueChange={props.onValueChange}>
       <SelectTrigger className="cell text-left">{props.options.find((option) => option.value === props.value)?.component}</SelectTrigger>
       {open && (
-        <SelectContent>
+        <SelectContent onClear={handleClear}>
           {props.options.map((option) => (
             <SelectItem key={option.value} value={option.value}>
               {option.component}
