3bda90365c4e988bd84c826afa6a7aca6efd744d
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-01-31 19:16:27 +0100

    Fixed spelling in emission column

--- optimistic/app/(routes)/[company]/_components/columns/co2e/header.tsx
+++ optimistic/app/(routes)/[company]/_components/columns/co2e/header.tsx
@@ -3,5 +3,5 @@ import { ActivityHeaderContext } from "../../../_utils/cell-types";
 import { Header } from "../../table-utils/headers/header";
 
 export const Co2eHeader: FC<ActivityHeaderContext<"co2e">> = (props) => {
-  return <Header ctx={props} title="CO²e emission" />;
+  return <Header ctx={props} title="CO2e emission" />;
 };
