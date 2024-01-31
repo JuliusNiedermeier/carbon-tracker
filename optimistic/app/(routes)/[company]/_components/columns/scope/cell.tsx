008992293abc2bde3b733f2f151c427fe0af75af
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-01-31 16:00:49 +0100

    Added scope numbers to scope column

--- optimistic/app/(routes)/[company]/_components/columns/scope/cell.tsx
+++ optimistic/app/(routes)/[company]/_components/columns/scope/cell.tsx
@@ -3,12 +3,24 @@ import { ActivityCellContext } from "@/app/(routes)/[company]/_utils/cell-types"
 import { SelectCell } from "../../table-utils/cells/select-cell";
 import { useScopes } from "../../../_hooks/use-scopes";
 import { Cell } from "../../cell";
+import { Badge } from "@/app/_components/ui/badge";
 
 export const ScopeCell: FC<ActivityCellContext<"scope.name">> = (props) => {
   const scopes = useScopes();
 
   const options = useMemo<ComponentProps<typeof SelectCell>["options"]>(
-    () => scopes.map((scope) => ({ value: scope.id.toString(), component: <span>{scope.name}</span> })),
+    () =>
+      scopes.map((scope) => ({
+        value: scope.id.toString(),
+        component: (
+          <div className="flex items-center gap-3 overflow-hidden">
+            <Badge>
+              {scope.scope}.{scope.subScope}
+            </Badge>
+            <span className="text-ellipsis overflow-hidden">{scope.name}</span>
+          </div>
+        ),
+      })),
     [scopes]
   );
 
