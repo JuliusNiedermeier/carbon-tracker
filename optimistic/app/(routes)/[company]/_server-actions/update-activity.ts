5b7f4070ffa072c4af93a781ed4153601dd49aed
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-02 12:08:32 +0100

    Return the updated nested activity from the update-activity server function

--- optimistic/app/(routes)/[company]/_server-actions/update-activity.ts
+++ optimistic/app/(routes)/[company]/_server-actions/update-activity.ts
@@ -3,10 +3,34 @@
 import { Activity, ActivityInsert } from "@/app/_database/schema";
 import { db } from "@/app/_services/postgres";
 import { eq } from "drizzle-orm";
+import { listActivities } from "./list-activities";
 
 export const updateActivity = async <Column extends keyof ActivityInsert>(ID: number, column: Column, value: ActivityInsert[Column]) => {
   await db
     .update(Activity)
     .set({ [column]: value })
     .where(eq(Activity.id, ID));
+
+  type ListActivitiesResult = Awaited<ReturnType<typeof listActivities>>[number];
+
+  const updatedActivity = await db.query.Activity.findFirst({
+    where: eq(Activity.id, ID),
+    with: {
+      scope: true,
+      unit: true,
+      factor: { columns: { embedding: false }, with: { unit: true } },
+      location: { columns: { name: true }, with: { company: { columns: { id: true, name: true } } } },
+    },
+  });
+
+  if (!updatedActivity) return null;
+
+  const formattedUpdatedActivity: ListActivitiesResult = {
+    ...updatedActivity,
+    locationName: updatedActivity.location.name,
+    companyId: updatedActivity.location.company.id,
+    companyName: updatedActivity.location.company.name,
+  };
+
+  return formattedUpdatedActivity;
 };
