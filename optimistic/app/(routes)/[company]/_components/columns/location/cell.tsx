14416d8283f1b11c2e550abaa0fecf85c81a9ec3
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-02 15:39:45 +0100

    Added location selector dialog

--- optimistic/app/(routes)/[company]/_components/columns/location/cell.tsx
+++ optimistic/app/(routes)/[company]/_components/columns/location/cell.tsx
@@ -1,7 +1,23 @@
-import { FC } from "react";
-import { Cell } from "@/app/(routes)/[company]/_components/cell";
 import { ActivityCellContext } from "@/app/(routes)/[company]/_utils/cell-types";
+import { Cell } from "@/app/(routes)/[company]/_components/cell";
+import { FC, useState } from "react";
+import { Dialog, DialogContent } from "@/app/_components/ui/dialog";
+import { cn } from "@/app/_utils/cn";
+import { DialogTrigger } from "@radix-ui/react-dialog";
+import { LocationSelector } from "../../location-selector/location-selector";
 
 export const LocationCell: FC<ActivityCellContext<"locationName">> = (props) => {
-  return <Cell width={props.column.getSize()}>{props.getValue()}</Cell>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <Cell width={props.column.getSize()} padding={false}>
+        <DialogTrigger className={cn("w-full h-full block whitespace-nowrap px-3 text-left")}>{props.getValue()}</DialogTrigger>
+      </Cell>
+
+      <DialogContent className="max-w-[90vw] h-[90vh] flex flex-col gap-0 p-0 border-none overflow-hidden" showCloseButton={false}>
+        <LocationSelector />
+      </DialogContent>
+    </Dialog>
+  );
 };
---
+++ optimistic/app/(routes)/[company]/_components/location-selector/location-selector.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+export const LocationSelector: FC = () => {
+  return <div>LocationSelector</div>;
+};
