60d8a1353d13a0d61f7581702e71c312cb514a83
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-02 15:19:38 +0100

    Added create activity action to activity creator button

--- optimistic/app/(routes)/[company]/_components/activity-creator/toolbar.tsx
+++ optimistic/app/(routes)/[company]/_components/activity-creator/toolbar.tsx
@@ -2,16 +2,21 @@ import { FC } from "react";
 import { useActivityCreator } from "../providers/activity-creator-provider";
 import { Button } from "@/app/_components/ui/button";
 import { ChevronUp, Lock } from "lucide-react";
+import { ActivityInsert } from "@/app/_database/schema";
 
 export const ActivityCreatorToolbar: FC<{ height: number }> = (props) => {
-  const { isValidCandidate } = useActivityCreator();
+  const { isValidCandidate, createActivity, candidate } = useActivityCreator();
 
   return (
     <div className="flex gap-1 items-center h-full bg-white rounded-md border p-1" style={{ height: props.height }}>
       <Button size="icon" variant="ghost" className="h-full">
         <Lock size="16" />
       </Button>
-      <Button className="h-full rounded-sm bg-emerald-600 text-emerald-200 gap-2 hover:bg-emerald-700" disabled={!isValidCandidate}>
+      <Button
+        className="h-full rounded-sm bg-emerald-600 text-emerald-200 gap-2 hover:bg-emerald-700"
+        disabled={!isValidCandidate}
+        onClick={() => createActivity(candidate as ActivityInsert)}
+      >
         Add activity <ChevronUp size="16" />
       </Button>
     </div>
