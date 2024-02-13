345d7b107091bfe1c3762e447279e75dfbd9e108
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-13 18:46:55 +0100

    Added use create-company-location hook

---
+++ optimistic/app/(routes)/[company]/_hooks/use-create-company-location.ts
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCompany } from "../_server-actions/create-company";
+import { createCompanyLocation } from "../_server-actions/create-company-location";
+
+export const useCreateCompanyLocation = () => {
+  const qc = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationKey: ["create-company-location"],
+
+    // Perform the network request
+    mutationFn: async (variables: { companyID: number; name: string }) => {
+      return await createCompanyLocation(variables.companyID, variables.name);
+    },
+
+    onSuccess: (data, variables) => {
+      qc.invalidateQueries({ queryKey: ["list-company-locations", variables.companyID] });
+    },
+  });
+
+  return mutate;
+};
