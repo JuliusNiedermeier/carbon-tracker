b806ff801de3b4a81bc810a0451fbc95c3bd87cc
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-13 18:48:28 +0100

    Removed unused code

--- optimistic/app/(routes)/[company]/_hooks/use-create-company.ts
+++ optimistic/app/(routes)/[company]/_hooks/use-create-company.ts
@@ -15,33 +15,6 @@ export const useCreateCompany = () => {
     onSuccess: () => {
       qc.invalidateQueries({ queryKey: ["get-corporate-group"] });
     },
-
-    // Optimistically update local activities
-    // onMutate: async (variables) => {
-    //   await qc.cancelQueries({ queryKey: ["list-activities", rootCompanySlug] });
-    //   const previousActivities = qc.getQueryData<Activity[]>(["list-activities", rootCompanySlug])?.map((activity) => ({ ...activity }));
-
-    //   qc.setQueryData<Activity[]>(["list-activities", rootCompanySlug], (oldActivities) => {
-    //     return oldActivities?.map((oldActivity) =>
-    //       oldActivity.id === variables.activityID ? { ...oldActivity, [variables.column]: variables.value } : oldActivity
-    //     );
-    //   });
-
-    //   return { previousActivities };
-    // },
-
-    // // Merge the updated activity into the activity set
-    // onSuccess: (updatedActivity, variables, context) => {
-    //   if (!context.previousActivities || !updatedActivity) return;
-    //   const mergedActivitySet = context.previousActivities.map((activity) => (activity.id === updatedActivity.id ? updatedActivity : activity));
-    //   qc.setQueryData<Activity[]>(["list-activities", rootCompanySlug], mergedActivitySet);
-    // },
-
-    // // Rollback the optimistic update
-    // onError: (error, newItem, context) => {
-    //   if (!context) return;
-    //   qc.setQueryData<Activity[]>(["list-activities", rootCompanySlug], context.previousActivities);
-    // },
   });
 
   return mutate;
