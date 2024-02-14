0aaba650b55fbf345647a8232d21c289c90b6827
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-14 13:40:48 +0100

    Use base cell component in AggregatedCell

--- optimistic/app/(routes)/[company]/_components/table-utils/cells/aggregated-cell.tsx
+++ optimistic/app/(routes)/[company]/_components/table-utils/cells/aggregated-cell.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { ActivityCellContext } from "../../../_utils/cell-types";
+import { Cell } from "../../cell";
 
 export const AggregatedCell: FC<ActivityCellContext<any>> = (props) => {
-  return <div className="cell">{props.getValue()}</div>;
+  return <Cell width={props.column.getSize()}>{props.getValue()}</Cell>;
 };
