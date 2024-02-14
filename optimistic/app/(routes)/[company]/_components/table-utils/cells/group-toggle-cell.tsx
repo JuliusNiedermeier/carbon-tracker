bdcc5b9827007f8801b0b81de38d7c9117d8b8d5
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-14 13:58:34 +0100

    Use base cell component instead of cell class in group-toggle-cell

--- optimistic/app/(routes)/[company]/_components/table-utils/cells/group-toggle-cell.tsx
+++ optimistic/app/(routes)/[company]/_components/table-utils/cells/group-toggle-cell.tsx
@@ -1,13 +1,20 @@
 import { ChevronsDownUp, ChevronsUpDown } from "lucide-react";
 import { FC } from "react";
 import { ActivityCellContext } from "../../../_utils/cell-types";
+import { Cell } from "../../cell";
 
 export const GroupToggleCell: FC<ActivityCellContext<any>> = (props) => {
   return (
-    <button className="cell flex items-center gap-2 !bg-gray-200" disabled={!props.row.getCanExpand()} onClick={props.row.getToggleExpandedHandler()}>
-      <span className="mr-auto font-medium">{props.getValue()}</span>
-      <span className="text-xs text-muted-foreground">{props.row.subRows.length}</span>
-      {props.row.getIsExpanded() ? <ChevronsDownUp size="16" /> : <ChevronsUpDown size="16" />}
-    </button>
+    <Cell width={props.column.getSize()} padding={false}>
+      <button
+        className="flex items-center gap-2 !bg-gray-200 w-full h-full px-3"
+        disabled={!props.row.getCanExpand()}
+        onClick={props.row.getToggleExpandedHandler()}
+      >
+        <span className="mr-auto font-medium">{props.getValue()}</span>
+        <span className="text-xs text-muted-foreground">{props.row.subRows.length}</span>
+        {props.row.getIsExpanded() ? <ChevronsDownUp size="16" /> : <ChevronsUpDown size="16" />}
+      </button>
+    </Cell>
   );
 };
