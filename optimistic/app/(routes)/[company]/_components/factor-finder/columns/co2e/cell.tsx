3196e66e82356f1dbb066fc55123d5f2403eaf2b
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-14 16:41:37 +0100

    Improved co2e value formatting and handling missing or NaN values in factor finder

--- optimistic/app/(routes)/[company]/_components/factor-finder/columns/co2e/cell.tsx
+++ optimistic/app/(routes)/[company]/_components/factor-finder/columns/co2e/cell.tsx
@@ -2,11 +2,19 @@ import { EmissionFactorFinderCellContext } from "@/app/(routes)/[company]/_utils
 import { FC } from "react";
 import { Cell } from "../../cell";
 import { numberFormat } from "@/app/_utils/number-formats";
+import { cn } from "@/app/_utils/cn";
+import { AlertTriangle } from "lucide-react";
 
 export const CO2eCell: FC<EmissionFactorFinderCellContext<"co2e">> = (props) => {
+  const isAvailable = typeof props.getValue() === "number" && !isNaN(props.getValue()!);
+
   return (
-    <Cell width={props.column.getSize()} className="flex-col justify-center items-end gap-1 text-foreground font-medium">
-      {numberFormat.format(Number(props.getValue()))}
+    <Cell
+      width={props.column.getSize()}
+      className={cn("flex justify-end items-center gap-2 text-foreground font-medium", { "text-muted-foreground font-normal text-orange-400": !isAvailable })}
+    >
+      {isAvailable ? numberFormat.format(Number(props.getValue())) : "Missing"}
+      {!isAvailable && <AlertTriangle size="16" />}
     </Cell>
   );
 };
--- optimistic/app/(routes)/[company]/_components/factor-finder/columns/co2e/footer.tsx
+++ optimistic/app/(routes)/[company]/_components/factor-finder/columns/co2e/footer.tsx
@@ -2,13 +2,22 @@ import { EmissionFactorFinderDisplayHeaderContext } from "@/app/(routes)/[compan
 import { FC } from "react";
 import { Cell } from "../../cell";
 import { useFactorFinder } from "../../factor-finder";
+import { numberFormat } from "@/app/_utils/number-formats";
+import { cn } from "@/app/_utils/cn";
+import { AlertTriangle } from "lucide-react";
 
 export const CO2eFooter: FC<EmissionFactorFinderDisplayHeaderContext> = (props) => {
   const { selectedFactorInfo } = useFactorFinder();
 
+  const isAvailable = typeof selectedFactorInfo?.co2e === "number" && !isNaN(selectedFactorInfo.co2e);
+
   return (
-    <Cell width={props.column.getSize()} className="flex-col justify-center items-start gap-1 font-medium">
-      {selectedFactorInfo?.year}
+    <Cell
+      width={props.column.getSize()}
+      className={cn("flex justify-end items-center gap-2 text-foreground font-medium", { "text-muted-foreground font-normal text-orange-400": !isAvailable })}
+    >
+      {isAvailable ? numberFormat.format(selectedFactorInfo.co2e!) : "Missing"}
+      {!isAvailable && <AlertTriangle size="16" />}
     </Cell>
   );
 };
