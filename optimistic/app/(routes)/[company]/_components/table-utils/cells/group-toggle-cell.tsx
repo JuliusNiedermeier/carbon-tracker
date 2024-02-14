a20cb5b67d3ad30949a902877ca5d373256902e4
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-14 18:34:35 +0100

    Added empty state in group-toggle-cell component

--- optimistic/app/(routes)/[company]/_components/table-utils/cells/group-toggle-cell.tsx
+++ optimistic/app/(routes)/[company]/_components/table-utils/cells/group-toggle-cell.tsx
@@ -11,7 +11,7 @@ export const GroupToggleCell: FC<ActivityCellContext<any>> = (props) => {
         disabled={!props.row.getCanExpand()}
         onClick={props.row.getToggleExpandedHandler()}
       >
-        <span className="mr-auto font-medium">{props.getValue()}</span>
+        <span className="mr-auto font-medium">{props.getValue() ?? "Empty"}</span>
         <span className="text-xs text-muted-foreground">{props.row.subRows.length}</span>
         {props.row.getIsExpanded() ? <ChevronsDownUp size="16" /> : <ChevronsUpDown size="16" />}
       </button>
