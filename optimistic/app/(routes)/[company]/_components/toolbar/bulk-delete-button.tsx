a801f2e1c1c4045d3a7189c496d5ebaa5202975f
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-15 14:30:18 +0100

    Use new use-delete-activities hook to bulk delete activities

--- optimistic/app/(routes)/[company]/_components/toolbar/bulk-delete-button.tsx
+++ optimistic/app/(routes)/[company]/_components/toolbar/bulk-delete-button.tsx
@@ -1,17 +1,20 @@
+"use client";
+
 import { Table } from "@tanstack/react-table";
-import { FC, useState } from "react";
+import { ComponentProps, FC, useState } from "react";
 import { Loader2, Trash } from "lucide-react";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/app/_components/ui/dialog";
 import { Button } from "@/app/_components/ui/button";
 import { Activity } from "../../_hooks/use-activities";
-import { deleteActivities } from "../../_server-actions/delete-activities";
+import { useDeleteActivities } from "../../_hooks/use-delete-activities";
+import { useActivityGrid } from "../providers/activity-grid-provider";
 
 interface Props {
   table: Table<Activity>;
 }
 
 export const BulkDeleteButton: FC<Props> = ({ table }) => {
-  const [loading, setLoading] = useState(false);
+  const { rootCompanySlug } = useActivityGrid();
   const [open, setOpen] = useState(false);
 
   const selectedCount = table.getSelectedRowModel().rows.length;
@@ -20,16 +23,12 @@ export const BulkDeleteButton: FC<Props> = ({ table }) => {
 
   const buttonLabel = `Delete ${selectedCount} ${selectedCount > 1 ? "rows" : "row"}`;
 
-  const handleDelete = async () => {
-    setLoading(true);
-    try {
-      await deleteActivities(selectedIds);
-    } catch (err) {
-      console.error(err);
-    } finally {
-      setLoading(false);
-      setOpen(false);
-    }
+  const { mutateAsync: deleteActivities, isPending: isDeleting } = useDeleteActivities();
+
+  const handleDeleteButtonClick: ComponentProps<typeof Button>["onClick"] = async () => {
+    await deleteActivities({ IDs: selectedIds, rootCompanySlug });
+    setOpen(false);
+    table.setRowSelection({});
   };
 
   return (
@@ -43,12 +42,14 @@ export const BulkDeleteButton: FC<Props> = ({ table }) => {
       <DialogContent className="w-[20rem]">
         <DialogHeader>
           <DialogTitle>Please confirm.</DialogTitle>
-          <DialogDescription>You are about to {buttonLabel}. You cannot restore them! Are you sure you want to delete all selected activities?</DialogDescription>
+          <DialogDescription>
+            You are about to {buttonLabel}. You cannot restore them! Are you sure you want to delete all selected activities?
+          </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button variant="destructive" className="w-full gap-4" onClick={handleDelete} disabled={loading}>
+          <Button variant="destructive" className="w-full gap-4" onClick={handleDeleteButtonClick} disabled={isDeleting}>
             {buttonLabel}
-            {loading && <Loader2 size={20} className="animate-spin" />}
+            {isDeleting && <Loader2 size={20} className="animate-spin" />}
           </Button>
         </DialogFooter>
       </DialogContent>
