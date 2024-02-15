7bda0f78395232feeed9b7c81e57b1fd07fb5404
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-15 19:01:36 +0100

    Created the factor column for the activity creator

---
+++ optimistic/app/(routes)/[company]/_components/columns/factor/footer.tsx
@@ -0,0 +1,29 @@
+import { ComponentProps, FC } from "react";
+import { ActivityHeaderContext } from "../../../_utils/cell-types";
+import { FactorBaseCell } from "./base-cell";
+import { useActivityCreator } from "../../providers/activity-creator-provider";
+import { useEmissionFactorInfo } from "../../../_hooks/use-emission-factor-info";
+
+export const FactorFooter: FC<ActivityHeaderContext<"factor.co2e">> = (props) => {
+  const { candidate, setCandidate } = useActivityCreator();
+
+  const { data: factorInfo } = useEmissionFactorInfo(candidate.emissionFactorId ?? null);
+
+  const handleSelectFactor: ComponentProps<typeof FactorBaseCell>["factorFinder"]["onSelect"] = (factorID) => {
+    setCandidate((candidate) => ({ ...candidate, emissionFactorId: factorID }));
+  };
+
+  return (
+    <FactorBaseCell
+      width={props.column.getSize()}
+      co2e={factorInfo?.co2e}
+      factorUnitID={factorInfo?.unit.id}
+      amountUnitID={candidate.unitId}
+      factorFinder={{
+        emissionFactorID: candidate.emissionFactorId ?? null,
+        onSelect: handleSelectFactor,
+        initialFilters: { searchTerm: candidate.description, amountUnitID: candidate.unitId },
+      }}
+    />
+  );
+};
