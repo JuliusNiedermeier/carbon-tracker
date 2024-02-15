14167122e7ded111b1b23a20bf6ca1c3caf48a3f
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-15 18:58:07 +0100

    Make the factor finder category footer work with the updated factor finder

--- optimistic/app/(routes)/[company]/_components/factor-finder/columns/category/footer.tsx
+++ optimistic/app/(routes)/[company]/_components/factor-finder/columns/category/footer.tsx
@@ -1,23 +1,17 @@
 import { EmissionFactorFinderDisplayHeaderContext } from "@/app/(routes)/[company]/_utils/cell-types";
-import { ComponentProps, FC } from "react";
+import { FC } from "react";
 import { Cell } from "../../cell";
 import { Category } from "./category";
 import { useFactorFinder } from "../../factor-finder";
 import { CheckCircle2, X } from "lucide-react";
 import { Button } from "@/app/_components/ui/button";
-import { useActivityGrid } from "@/app/(routes)/[company]/_components/providers/activity-grid-provider";
 
 export const CategoryFooter: FC<EmissionFactorFinderDisplayHeaderContext> = (props) => {
-  const { selectedFactorInfo, activityID } = useFactorFinder();
-  const { updateCell } = useActivityGrid();
-
-  const handleUnlinkFactorClick: ComponentProps<typeof Button>["onClick"] = () => {
-    updateCell(activityID, "emissionFactorId", null);
-  };
+  const { selectedFactorInfo, setSelectedFactorID } = useFactorFinder();
 
   return (
     <Cell className="flex-1 gap-4 pl-4" padding={false}>
-      <Button variant="secondary" className="gap-2 hover:text-red-600" onClick={handleUnlinkFactorClick}>
+      <Button variant="secondary" className="gap-2 hover:text-red-600" onClick={() => setSelectedFactorID(null)}>
         <X className="" size="16" />
         Unlink factor
       </Button>
