c453deed5784f0d02ad76dec70409b1297fb70df
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-15 15:20:49 +0100

    Removed dash from blank footer cell

--- optimistic/app/(routes)/[company]/_components/table-utils/cells/blank-cell.tsx
+++ optimistic/app/(routes)/[company]/_components/table-utils/cells/blank-cell.tsx
@@ -1,11 +1,7 @@
 import { FC } from "react";
 import { Cell } from "../../cell";
-import { ActivityDisplayHeaderContext, ActivityHeaderContext } from "../../../_utils/cell-types";
+import { ActivityDisplayHeaderContext } from "../../../_utils/cell-types";
 
 export const BlankCell: FC<ActivityDisplayHeaderContext> = (props) => {
-  return (
-    <Cell width={props.header.getSize()} className="pointer-events-none">
-      -
-    </Cell>
-  );
+  return <Cell width={props.header.getSize()} className="pointer-events-none"></Cell>;
 };
