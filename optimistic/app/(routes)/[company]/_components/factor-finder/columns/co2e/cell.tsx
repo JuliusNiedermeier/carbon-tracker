f761e391599dbcfe86d724e7fde6398657228ab6
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-15 19:00:55 +0100

    Make use of the new EmissionFactorValue component in the factor finder co2e footer

--- optimistic/app/(routes)/[company]/_components/factor-finder/columns/co2e/cell.tsx
+++ optimistic/app/(routes)/[company]/_components/factor-finder/columns/co2e/cell.tsx
@@ -1,20 +1,12 @@
 import { EmissionFactorFinderCellContext } from "@/app/(routes)/[company]/_utils/cell-types";
 import { FC } from "react";
 import { Cell } from "../../cell";
-import { numberFormat } from "@/app/_utils/number-formats";
-import { cn } from "@/app/_utils/cn";
-import { AlertTriangle } from "lucide-react";
+import { EmissionFactorValue } from "../../../columns/factor/emission-factor-value";
 
 export const CO2eCell: FC<EmissionFactorFinderCellContext<"co2e">> = (props) => {
-  const isAvailable = typeof props.getValue() === "number" && !isNaN(props.getValue()!);
-
   return (
-    <Cell
-      width={props.column.getSize()}
-      className={cn("flex justify-end items-center gap-2 text-foreground font-medium", { "text-muted-foreground font-normal text-orange-400": !isAvailable })}
-    >
-      {isAvailable ? numberFormat.format(Number(props.getValue())) : "Missing"}
-      {!isAvailable && <AlertTriangle size="16" />}
+    <Cell width={props.column.getSize()}>
+      <EmissionFactorValue value={props.getValue()} />
     </Cell>
   );
 };
