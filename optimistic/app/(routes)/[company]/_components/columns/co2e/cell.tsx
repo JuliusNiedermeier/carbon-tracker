360d0e38a50990bc57f06e0621d364d4b55b19e8
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-18 18:09:15 +0100

    Display the co2e emission saved on the activity

--- optimistic/app/(routes)/[company]/_components/columns/co2e/cell.tsx
+++ optimistic/app/(routes)/[company]/_components/columns/co2e/cell.tsx
@@ -4,17 +4,9 @@ import { ActivityCellContext } from "@/app/(routes)/[company]/_utils/cell-types"
 import { numberFormat } from "@/app/_utils/number-formats";
 
 export const Co2eCell: FC<ActivityCellContext<"co2e">> = (props) => {
-  const co2eFactor = props.row.original.factor?.co2e;
-  const amount = props.row.original.amount;
-
-  const hasFactor = typeof co2eFactor === "number";
-  const hasAmount = typeof amount === "number";
-
-  const emission = hasFactor && hasAmount && numberFormat.format(amount * co2eFactor);
-
   return (
     <Cell width={props.column.getSize()} className="justify-end">
-      {emission}
+      {props.getValue() !== null && numberFormat.format(props.getValue()!)}
     </Cell>
   );
 };
