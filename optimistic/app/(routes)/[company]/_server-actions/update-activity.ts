e680cd43b387c56e2f4a8700a3139cf7d18e4bdf
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-18 18:08:02 +0100

    Added automatic co2e emission update in update-activity server function

--- optimistic/app/(routes)/[company]/_server-actions/update-activity.ts
+++ optimistic/app/(routes)/[company]/_server-actions/update-activity.ts
@@ -4,14 +4,27 @@ import { Activity, ActivityInsert } from "@/app/_database/schema";
 import { db } from "@/app/_services/postgres";
 import { eq } from "drizzle-orm";
 import { listActivities } from "./list-activities";
+import { Input, number, object, safeParse } from "valibot";
+import { evaluateFormula } from "../_utils/evaluate-formula";
 
-export const updateActivity = async <Column extends keyof ActivityInsert>(ID: number, column: Column, value: ActivityInsert[Column]) => {
-  await db
-    .update(Activity)
-    .set({ [column]: value })
-    .where(eq(Activity.id, ID));
+const co2eEmissionCalculationConfig = {
+  dependencies: ["amount", "amountFormula", "unitId", "emissionFactorId"] satisfies (keyof ActivityInsert)[],
+  requiredActivityPropertiesSchema: object({ amount: number(), unitID: number(), factorUnitID: number(), co2e: number() }),
+};
 
-  type ListActivitiesResult = Awaited<ReturnType<typeof listActivities>>[number];
+type RequiredSchemaForEmissionCo2eEmissionCalculation = Record<keyof Input<typeof co2eEmissionCalculationConfig.requiredActivityPropertiesSchema>, any>;
+type ListActivitiesResult = Awaited<ReturnType<typeof listActivities>>[number];
+
+export const updateActivity = async <Column extends keyof ActivityInsert>(
+  ID: number,
+  column: Column,
+  value: ActivityInsert[Column]
+): Promise<ListActivitiesResult | null> => {
+  const update: Partial<ActivityInsert> = { [column]: value };
+
+  if (column === "amountFormula") update.amount = value !== null ? evaluateFormula(value as NonNullable<ActivityInsert["amountFormula"]>) : null;
+
+  await db.update(Activity).set(update).where(eq(Activity.id, ID));
 
   const updatedActivity = await db.query.Activity.findFirst({
     where: eq(Activity.id, ID),
@@ -32,5 +45,18 @@ export const updateActivity = async <Column extends keyof ActivityInsert>(ID: nu
     companyName: updatedActivity.location.company.name,
   };
 
-  return formattedUpdatedActivity;
+  if (!co2eEmissionCalculationConfig.dependencies.includes(column as any)) return formattedUpdatedActivity;
+
+  const { success: hasCo2eEmissionUpdateProperties, output } = safeParse(co2eEmissionCalculationConfig.requiredActivityPropertiesSchema, {
+    amount: updatedActivity.amount,
+    unitID: updatedActivity.unitId,
+    factorUnitID: updatedActivity.factor?.unitId,
+    co2e: updatedActivity.factor?.co2e,
+  } satisfies RequiredSchemaForEmissionCo2eEmissionCalculation);
+
+  const updatedCo2e = hasCo2eEmissionUpdateProperties && output.unitID === output.factorUnitID ? output.amount * output.co2e : null;
+
+  await db.update(Activity).set({ co2e: updatedCo2e }).where(eq(Activity.id, ID));
+
+  return { ...formattedUpdatedActivity, co2e: updatedCo2e };
 };
