a5b87dac690a93dde0a25ebebeadd6404125bda4
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-22 15:28:15 +0100

    Fixed optimistic update interference. Added activity and column scoped debounce timeouts

--- optimistic/app/(routes)/[company]/_hooks/use-update-activity.ts
+++ optimistic/app/(routes)/[company]/_hooks/use-update-activity.ts
@@ -9,18 +9,23 @@ export const useUpdateActivity = (rootCompanySlug: string) => {
   // The debounce logic must differentiate between update calls to different columns.
   // Currently any column update is canceled immediately by every other column update.
   // A timeout map with a key for every coluzmn sounds like a viable solution to this problem.
-  const updateTimeout = useRef<NodeJS.Timeout | null>(null);
+
+  // Ref to store timeouts, keyed by a combination of activity ID and column name
+  const updateTimeouts = useRef<Record<string, NodeJS.Timeout | null>>({});
 
   const { mutate } = useMutation({
     mutationKey: ["update-activity", rootCompanySlug],
 
     // Perform the network request
     mutationFn: async (variables: { activityID: Activity["id"]; column: keyof Activity; value: Activity[keyof Activity] }) => {
-      // Clear any existing timeout
-      if (updateTimeout.current) clearTimeout(updateTimeout.current);
+      // Create a unique key for each activity-column combination
+      const timeoutKey = `${variables.activityID}:${variables.column}`;
+
+      // Clear any existing timeout for this activity-column combination
+      if (updateTimeouts.current[timeoutKey]) clearTimeout(updateTimeouts.current[timeoutKey]!);
 
       return new Promise<Awaited<ReturnType<typeof updateActivity>>>((resolve) => {
-        updateTimeout.current = setTimeout(async () => {
+        updateTimeouts.current[timeoutKey] = setTimeout(async () => {
           // TODO: Remove any type
           const updatedActivity = await updateActivity(variables.activityID, variables.column as any, variables.value);
           resolve(updatedActivity);
@@ -45,10 +50,11 @@ export const useUpdateActivity = (rootCompanySlug: string) => {
     },
 
     // Merge the updated activity into the activity set
-    onSuccess: (updatedActivity, variables, context) => {
-      if (!context.previousActivities || !updatedActivity) return;
-      const mergedActivitySet = context.previousActivities.map((activity) => (activity.id === updatedActivity.id ? updatedActivity : activity));
-      qc.setQueryData<Activity[]>(["list-activities", rootCompanySlug], mergedActivitySet);
+    onSuccess: (updatedActivity) => {
+      if (!updatedActivity) return;
+      qc.setQueryData<Activity[]>(["list-activities", rootCompanySlug], (currentActivities) => {
+        return currentActivities?.map((activity) => (activity.id === updatedActivity.id ? updatedActivity : activity));
+      });
     },
 
     // Rollback the optimistic update
