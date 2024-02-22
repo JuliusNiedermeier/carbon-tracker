e33f9e899237b0e2c94698ea7d0de2dc59630ab0
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-23 00:16:12 +0100

    Use the base group toggle cell inside the default group toggle cell

--- optimistic/app/(routes)/[company]/_components/table-utils/cells/group-toggle-cell.tsx
+++ optimistic/app/(routes)/[company]/_components/table-utils/cells/group-toggle-cell.tsx
@@ -1,20 +1,16 @@
-import { ChevronsDownUp, ChevronsUpDown } from "lucide-react";
 import { FC } from "react";
 import { ActivityCellContext } from "../../../_utils/cell-types";
-import { Cell } from "../../cell";
+import { GroupToggleCellBase } from "./group-toggle-cell-base";
 
 export const GroupToggleCell: FC<ActivityCellContext<any>> = (props) => {
   return (
-    <Cell width={props.column.getSize()} padding={false}>
-      <button
-        className="flex items-center gap-2 !bg-gray-200 w-full h-full px-3"
-        disabled={!props.row.getCanExpand()}
-        onClick={props.row.getToggleExpandedHandler()}
-      >
-        <span className="mr-auto font-medium">{props.getValue() ?? "Empty"}</span>
-        <span className="text-xs text-muted-foreground">{props.row.subRows.length}</span>
-        {props.row.getIsExpanded() ? <ChevronsDownUp size="16" /> : <ChevronsUpDown size="16" />}
-      </button>
-    </Cell>
+    <GroupToggleCellBase
+      width={props.column.getSize()}
+      label={props.getValue() ?? "Empty"}
+      subItemCount={props.row.subRows.length}
+      expanded={props.row.getIsExpanded()}
+      disabled={!props.row.getCanExpand()}
+      onClick={props.row.getToggleExpandedHandler()}
+    />
   );
 };
