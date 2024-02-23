406ea560b0b6d0036b3e2b3ea4a30b0a442da33c
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-02-23 01:09:20 +0100

    Added pinned prop to cell component that can make the cell sticky either left or right

--- optimistic/app/(routes)/[company]/_components/cell.tsx
+++ optimistic/app/(routes)/[company]/_components/cell.tsx
@@ -1,7 +1,9 @@
 import { cn } from "@/app/_utils/cn";
+import { ColumnPinningPosition } from "@tanstack/react-table";
 import { ComponentProps, PropsWithChildren, forwardRef } from "react";
 
 type Props = ComponentProps<"div"> & {
+  pinned?: ColumnPinningPosition;
   width?: number;
   padding?: boolean;
 };
@@ -11,8 +13,8 @@ export const Cell = forwardRef<HTMLDivElement, PropsWithChildren<Props>>(({ chil
     <div
       ref={ref}
       className={cn(
-        "relative flex h-full items-center bg-transparent whitespace-nowrap overflow-hidden text-ellipsis cursor-default text-sm text-gray-600 border-r border-gray-300 last:border-r-0 focus-within:ring-2 focus-within:ring-gray-600 focus-within:!border-transparent hover:bg-gray-200 focus-within:bg-gray-200 focus-within:z-10 focus-within:rounded-[1px] focus-within:text-gray-800 focus-within:font-medium focus-within:cursor-auto outline-none",
-        { "px-3": padding ?? true },
+        "relative flex h-full items-center bg-white whitespace-nowrap overflow-hidden text-ellipsis cursor-default text-sm text-gray-600 border-r border-gray-300 last:border-r-0 focus-within:ring-2 focus-within:ring-gray-600 focus-within:!border-transparent hover:bg-gray-200 focus-within:bg-gray-200 focus-within:z-10 focus-within:rounded-[1px] focus-within:text-gray-800 focus-within:font-medium focus-within:cursor-auto outline-none",
+        { "px-3": padding ?? true, "sticky z-10": props.pinned, "left-0": props.pinned === "left", "right-0": props.pinned === "right" },
         className
       )}
       style={{ width: `${width || 300}px` }}
